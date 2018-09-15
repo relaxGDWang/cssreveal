a79a89321ab28f8e84da98713b8875d433517629
relaxGD <relax19830426@gmail.com>
2018-09-15 12:27:45 +0800

    write the 3 exp

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var showData={
-    e1:{title:'透明边框',des:'background-clip 属性不被IE8及以下版本的浏览器所支持'},
-    e2:{title:'多边框',des:'目前'}
+    e1:{title:'透明边框',des:'background-clip 属性不被IE8及以下版本的浏览器所支持。'},
+    e2:{title:'多重边框',des:'真所谓鱼和熊掌不可兼得，多层边框借助box-shadow实现，但无法支持边框样式，而支持边框样式的out-line则不支持多边框设置和圆角响应。另外请留意他们的hover响应范围之外，还要注意box-shadow的透明来实现多层透明边框会导致颜色重叠的问题。'},
+    e3:{title:'灵活的背景定位',des:''}
 };
-var nowSel='e2';
+var nowSel='e3';
 
 var expt= {
   el: "#app",
